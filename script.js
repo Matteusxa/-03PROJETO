c4496ac29cc7fee558151e439d128f7ba34d0f98
matteusxa <mateusfamilia304@gmail.com>
2023-10-31 21:36:28 -0300

    segundo-commit

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ function generateNumber(){
    const min = Math.ceil(document.querySelector('.elemento1').value)
    const max = Math.floor(document.querySelector('.elemento2').value)
 
-    const result =  Math.floor(Math.random() * (max - min + 1)) + min;
+    if(min >= max){
+        alert('O valor minimo tem quer ser MENOR que o valor MÁXIMO')
+    } else{
+        const result =  Math.floor(Math.random() * (max - min + 1)) + min;
     
-    alert(result)
+        alert(result)
+    }
+
+
+  
 }
